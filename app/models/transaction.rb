bf272c4689d6b994e18deda8eef89fd4043378fe
saransh-bajpai <saranshbjpai@gmail.com>
2023-10-24 16:13:30 +0530

    validations added

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,14 @@
 class Transaction < ApplicationRecord
+  validates_presence_of :transaction_description, :activity_id, :transaction_time
+
+  CURRENCY = ['INR', 'USD']
+  validates :transaction_currency, inclusion: { in: CURRENCY }
+
+
+  TRANSACTION_METHOD = ['CREDIT', 'DEBIT']
+  validates :transaction_method, inclusion: { in: TRANSACTION_METHOD }
+
+  validates :transaction_amount, numericality: { greater_than: 0.0 }
+
+  validates :account_balance, numericality: true
 end
