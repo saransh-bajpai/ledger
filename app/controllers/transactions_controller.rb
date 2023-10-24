0f0a531cf03408cde34587897eb6832f2285c564
saransh-bajpai <saranshbjpai@gmail.com>
2023-10-24 17:42:21 +0530

    params added

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -9,12 +9,14 @@ class TransactionsController < ApplicationController
     if transaction.save
       render json: transaction, status: :created
     else
-      render json: transaction.errors, status: :unprocessable_entity
+      render json: transaction.errors, status: :bad_request
     end
 
   end
 
   def transaction_params
-    params.require(:transaction).permit(:transaction_description)
+    params.require(:transaction).permit(:transaction_description, :activity_id, :transaction_time,
+                                        :transaction_currency, :transaction_amount, :transaction_method,
+                                        :account_balance, :requester, :source, :destination, :currency)
   end
 end
